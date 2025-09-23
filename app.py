72d00ef31c63c2a8719fcba962f07264174eca7c
Nicolas Murphy <murphynick10@gmail.com>
2025-09-22 22:28:12 -0500

    chore: remove dead code from app.py

--- app.py
+++ app.py
@@ -41,25 +41,19 @@ def serve_index():
     return send_from_directory("static", "index.html")
 
 
+app.route("/history", methods=["GET"])(history)
+app.route("/massbank/<algorithm>", methods=["POST"])(generate_audio_with_data)
+app.route("/custom/<algorithm>", methods=["POST"])(generate_audio_with_custom_data)
+app.route("/popular", methods=["GET"])(popular)
+
+
 @app.route("/<path:path>")
 def serve_static_or_spa(path):
-    if (
-        path.startswith("api/")
-        or path in ["history", "popular"]
-        or path.startswith("massbank/")
-    ):
-        return app.send_static_file(path)
-
     try:
         return send_from_directory("static", path)
     except FileNotFoundError:
         return send_from_directory("static", "index.html")
 
-
-app.route("/history", methods=["GET"])(history)
-app.route("/massbank/<algorithm>", methods=["POST"])(generate_audio_with_data)
-app.route("/custom/<algorithm>", methods=["POST"])(generate_audio_with_custom_data)
-app.route("/popular", methods=["GET"])(popular)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
