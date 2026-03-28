edd31dd1ee96089d499546d3862332a76f6b79ab
Nicolas Murphy <murphynick10@gmail.com>
2026-03-27 22:00:47 -0500

    Revert "Optimize base64ToBlob by writing directly to Uint8Array"
    This reverts commit 5f9ed30eb283459d30fc5cbaa36f951fafd9cb4c.

--- frontend/src/utils.ts
+++ frontend/src/utils.ts
@@ -3,11 +3,12 @@ export default function base64ToBlob(
   contentType = "audio/wav"
 ): Blob {
   const byteCharacters = atob(base64String);
-  const byteArray = new Uint8Array(byteCharacters.length);
+  const byteNumbers = new Array(byteCharacters.length);
 
   for (let i = 0; i < byteCharacters.length; i++) {
-    byteArray[i] = byteCharacters.charCodeAt(i);
+    byteNumbers[i] = byteCharacters.charCodeAt(i);
   }
 
+  const byteArray = new Uint8Array(byteNumbers);
   return new Blob([byteArray], { type: contentType });
 }
